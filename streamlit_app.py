88803d382adabca42c274990dfb35f69b9eaa3af
tusharwadhwani <102729135+tusharwadhwani@users.noreply.github.com>
2022-08-29 19:18:39 +0530

    Update streamlit_app.py
    added examples to display in multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,14 @@ streamlit.title('My Mom\'s New Healthy Diner ')
 streamlit.header('🥣 Breakfast Menu') 
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 🍞Hard-Boiled Free-Range Egg')
-                
+streamlit.text('🐔 🍞Hard-Boiled Free-Range Egg')                
                 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
